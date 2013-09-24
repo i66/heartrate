4476e50c87686b6c65eb61f64ff280cc6de2410f
Shuo-Hung Chen <shuohong@gmail.com>
2013-09-24 19:48:51 +0800

    Create a gauge with SonicGauge

--- www/js/index.js
+++ www/js/index.js
@@ -33,17 +33,8 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        console.log('deviceready');
+        var defaultGauge = $('#default').SonicGauge();
+        defaultGauge.SonicGauge('val', 60);
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
 };
