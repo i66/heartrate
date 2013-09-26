efa5cd795a26c147c3583a6b3c9130098e370f25
Shuo-Hung Chen <shuohong@gmail.com>
2013-09-26 11:38:40 +0800

    First build for ios

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -33,17 +33,93 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        console.log('deviceready');
+        var bpmMeter = $('#bpm_meter').SonicGauge({
+            label   : 'BPM',
+            start   : {angle: -225, num: 0},
+            end     : {angle: 45, num: 220},
+            markers : [
+                {
+                    gap: 20,
+                    line: {"width": 20, "stroke": "none", "fill": "#eeeeee"},
+                    text: {"space": 22, "text-anchor": "middle", "fill": "#333333", "font-size": 18}
+                },{
+                    gap: 10, 
+                    line: {"width": 12, "stroke": "none", "fill": "#aaaaaa"},
+                    text: {"space": 18, "text-anchor": "middle", "fill": "#333333", "font-size": 12}
+                },{
+                    gap: 5, 
+                    line: {"width": 8, "stroke": "none", "fill": "#999999"}
+                }
+            ],
+            animation_speed : 300,
+            diameter: 400
+        });
+        var zoneMeter = $('#zone_meter').SonicGauge({
+            label   : 'Zone (%)',
+            start   : {angle: -225, num: 0},
+            end     : {angle: 45, num: 100},
+            markers : [
+                {
+                    gap: 10,
+                    line: {"width": 20, "stroke": "none", "fill": "#eeeeee"},
+                    text: {"space": 22, "text-anchor": "middle", "fill": "#333333", "font-size": 18}
+                },{
+                    gap: 5, 
+                    line: {"width": 8, "stroke": "none", "fill": "#999999"}
+                }
+            ],
+            animation_speed : 300,
+            diameter: 400
+        });
+        bpmMeter.SonicGauge('val', 90);
+        zoneMeter.SonicGauge('val', 90);
+
+        var userAge = 27;
+        var userBpm = 60;
+
+        $("#bpm_form").submit(function(e){
+            e.preventDefault();
+            setTimeout(incrementHeartRate, 200);
+        });
+
+        function incrementHeartRate(){
+            userBpm += 3;
+            zone = heartRateZone(userAge, userBpm);
+            bpmMeter.SonicGauge('val', userBpm);
+            zoneMeter.SonicGauge('val', zone['percentage']);
+            if (userBpm < 200) {
+                setTimeout(incrementHeartRate, 200);
+            }
+        }
+        setTimeout(incrementHeartRate, 200);
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
 };
+
+function heartRateZone(age, bpm) {
+    var maxBpm = 220 - age;
+    var zone = 1;
+    var percentage = 0;
+    var zone2Min = maxBpm * 0.6;
+    var zone3Min = maxBpm * 0.7;
+    var zone4Min = maxBpm * 0.8;
+    var zone5Min = maxBpm * 0.9;
+    if (bpm < zone2Min) {
+        zone = 1;
+        zonePercentage = (bpm / zone2Min) * 100;
+    } else if (bpm >= zone2Min && bpm < zone3Min) {
+        zone = 2;
+        zonePercentage = (bpm - zone2Min) / (maxBpm * 0.1 ) * 100;
+    } else if (bpm >= zone3Min && bpm < zone4Min) {
+        zone = 3;
+        zonePercentage = (bpm - zone3Min) / (maxBpm * 0.1 ) * 100;
+    } else if (bpm >= zone4Min && bpm < zone5Min) {
+        zone = 4;
+        zonePercentage = (bpm - zone4Min) / (maxBpm * 0.1 ) * 100;
+    } else if (bpm >= zone5Min) {
+        zone = 5;
+        zonePercentage = (bpm - zone5Min) / (maxBpm * 0.1 ) * 100;
+    }
+    return {'number': zone, 'percentage': zonePercentage.toFixed(0)};
+}
+
